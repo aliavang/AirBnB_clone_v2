8c08b2bf2a712b3843b202c4559abbf34d9ad1a7
aliavang <714@holbertonschool.com>
2019-12-17 21:59:40 +0000

    Updated attributes for DB transfer

--- models/state.py
+++ models/state.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state', cascade='all, delete-orphan')
+"""    @property
+    def cities(self):
+        Return list of City instances with state_id equal to current
+        State.id
+        return models.City.name"""
