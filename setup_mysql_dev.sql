de8781e028a60e0cff445b5a75a022918d76fe87
Brent Muha <800@holbertonschool.com>
2019-12-17 16:44:48 +0000

    Add development and test databases and users

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
--- Prepare a MySQL server for the project
+-- Create a development database and user
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS hbnb_dev@localhost;
 SET PASSWORD FOR hbnb_dev@localhost = 'hbnb_dev_pwd';
