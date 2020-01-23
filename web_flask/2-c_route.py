e2dabd958d1520edc2e6de825cb4b3b1c3e375db
aliavang <714@holbertonschool.com>
2020-01-23 07:48:55 +0000

    First commit, task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Script starting Flask web app with three routes
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Print hello hbnb"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Print hbnb"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """Print C followed by text"""
+    msg = text.replace('_', ' ')
+    return msg
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
