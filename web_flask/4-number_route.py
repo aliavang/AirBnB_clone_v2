5d3dac62e855c87afcaa77889edef24028619fa9
aliavang <714@holbertonschool.com>
2020-01-23 08:01:03 +0000

    First commit, task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Script starting Flask web app with five routes
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Print hello hbnb"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Print hbnb"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """Print C followed by text"""
+    msg = text.replace('_', ' ')
+    return "C {}".format(msg)
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def py(text="is cool"):
+    """Print Python followed by text"""
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def num(n):
+    """Print number if n is integer"""
+    if type(n) == int:
+        return "{} is a number".format(n)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
